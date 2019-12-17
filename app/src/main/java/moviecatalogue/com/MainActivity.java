3238daa5a73df30f745756a4196f76000ae7b9f4
Ramadansyah Munthe <ramadansyahym7@gmail.com>
2019-12-18 01:36:09 +0700

    submission2

--- app/src/main/java/moviecatalogue/com/MainActivity.java
+++ app/src/main/java/moviecatalogue/com/MainActivity.java
@@ -1,71 +1,93 @@
 package moviecatalogue.com;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
-import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuItem;
 
-import java.util.ArrayList;
+import com.google.android.material.tabs.TabLayout;
 
-import moviecatalogue.com.Activity.DetaiMovieActivity;
-import moviecatalogue.com.Adpater.MovieAdapter;
-import moviecatalogue.com.Data.Movie;
+import moviecatalogue.com.Adpater.SectionsPagerAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String [] title;
-    private String [] descriptio;
-    private TypedArray poster;
-    private MovieAdapter mAdapter;
-    private ArrayList<Movie> listMovie;
+//    private String [] title;
+//    private String [] descriptio;
+//    private TypedArray poster;
+//    private MovieAdapter mAdapter;
+//    private ArrayList<Movie> listMovie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAdapter = new MovieAdapter(this);
-        ListView lvList = findViewById(R.id.lv_movie);
-        lvList.setAdapter(mAdapter);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
 
-        prepare();
+        getSupportActionBar().setElevation(0);
 
-        addItem();
-
-        lvList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Movie movie = listMovie.get(position);
-
-                Intent intent = new Intent(MainActivity.this, DetaiMovieActivity.class);
-                intent.putExtra(DetaiMovieActivity.EXTRAMOVIE, movie);
-
-                startActivity(intent);
-            }
-        });
+//        mAdapter = new MovieAdapter(this);
+//        ListView lvList = findViewById(R.id.lv_movie);
+//        lvList.setAdapter(mAdapter);
+//
+//        prepare();
+//
+//        addItem();
+//
+//        lvList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                Movie movie = listMovie.get(position);
+//
+//                Intent intent = new Intent(MainActivity.this, DetaiMovieActivity.class);
+//                intent.putExtra(DetaiMovieActivity.EXTRAMOVIE, movie);
+//
+//                startActivity(intent);
+//            }
+//        });
     }
 
-    private void addItem() {
-       listMovie = new ArrayList<>();
-
-       for (int i = 0; i < title.length; i++){
-           Movie movie = new Movie();
-           movie.setTitle(title[i]);
-           movie.setDescription(descriptio[i]);
-           movie.setPoster(poster.getResourceId(i, -1));
-           listMovie.add(movie);
-       }
-
-       mAdapter.setListMovie(listMovie);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
-    private void prepare() {
-        title = getResources().getStringArray(R.array.title);
-        descriptio = getResources().getStringArray(R.array.descriptions);
-        poster = getResources().obtainTypedArray(R.array.poster);
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        if (item.getItemId() == R.id.settings){
+            Intent intent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
     }
+    //    private void addItem() {
+//       listMovie = new ArrayList<>();
+//
+//       for (int i = 0; i < title.length; i++){
+//           Movie movie = new Movie();
+//           movie.setTitle(title[i]);
+//           movie.setDescription(descriptio[i]);
+//           movie.setPoster(poster.getResourceId(i, -1));
+//           listMovie.add(movie);
+//       }
+//
+//       mAdapter.setListMovie(listMovie);
+//    }
+//
+//    private void prepare() {
+//        title = getResources().getStringArray(R.array.title);
+//        descriptio = getResources().getStringArray(R.array.descriptions);
+//        poster = getResources().obtainTypedArray(R.array.poster);
+//    }
 }
